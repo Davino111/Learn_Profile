fdc80141bbfc69caaf7015efeca2b158c5cb7b65
Davinoo <dimasalvino137@gmail.com>
2025-09-22 23:16:59 +0700

    darkmode

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("toggle-theme");
+const body = document.body;
+const moon = btn.querySelector(".fa-moon");
+const sun = btn.querySelector(".fa-sun");
+ 
+
+btn.addEventListener("click", () =>{
+
+    console.log("Hello world!");    
+    body.classList.toggle("darkmode");
+
+    if(body.classList.contains("darkmode")){
+        moon.style.display = "none";
+        sun.style.display = "inline-block";
+        sun.style.color = "white"
+       
+    }else{
+        moon.style.display = "inline-block";
+        sun.style.display = "none";
+        
+    }
+})
